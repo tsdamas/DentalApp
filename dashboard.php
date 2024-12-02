071df22440b3f137917cf5adee76e48704c3ec03
tsdamas <tssdamasceno@gmail.com>
2024-12-01 16:51:06 -0800

    add appointments list

--- dashboard.php
+++ dashboard.php
@@ -89,8 +89,8 @@ if (mysqli_num_rows($result) == 1) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="./stylesheet.css">
     </head>
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+<body class="bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-white">
   <div class="container-fluid">
     <a class="navbar-dental" href="#">Dental#</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -105,22 +105,38 @@ if (mysqli_num_rows($result) == 1) {
     </div>
   </div>
 </nav>
-    <div class="container text-center">
-  <div class="row">
-    <div class="col-lg-6 col-xs-12">
-        <h4>Today's Appointments</h4>
+    <div class="container">
         <div class="row">
-            <div class="col-lg-6">
-                <p><?php echo "$num_app"; ?> Appointments</p>
-                
+            <div class="col-lg-12 col-xs-12" id="app-summary">
+                <h5 class="mt-4">Good afternoon, <?php echo "$f_name"; ?>!</h5>
+                <h6 class="text-secondary"><?php echo date('l, F j, Y'); ?> </h6>
             </div>
-            <div class="col-lg-6">
-                <p><?php echo "$get_number_proc"; ?> Procedures</p>
+            
+        </div>
+  <div class="row">
+    <div class="col-lg-6 col-xs-12 rounded shadow-sm p-3 mt-5 mb-5 bg-white" id="app-summary">
+        <h5 class="mb-4">Today's Summary</h5>
+        <div class="row text-center">
+            <div class="row">
+            <div class="col-lg-6 col-sm-6" id="app-in-container">
+                <h5 class="text-secondary">Appointments</h5>
+            </div>
+                <div class="col-lg-6 col-sm-6" id="app-in-container">
+                <h5 class="text-secondary">Procedures</h5>
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-lg-6 col-sm-6" id="app-in-container">
+                <h6><?php echo "$num_app"; ?></h6>
+            </div>
+               <div class="col-lg-6 col-sm-6"  id="app-in-container">
+                <h6><?php echo "$get_number_proc"; ?></h6>
+            </div>
             </div>
         </div>
     </div>
     <div class="col-lg-6 col-xs-12">
-      2 of 2
+
     </div>
   </div>
   <div class="row">
@@ -128,13 +144,13 @@ if (mysqli_num_rows($result) == 1) {
       <table class="table table-light table-striped table-hover">
   <thead>
     <tr>
-      <th scope="col">Appointment Id</th>
-      <th scope="col">Patient Name</th>
-      <th scope="col">Dentist Name</th>
-      <th scope="col">Room Number</th>
+      <th scope="col" class="text-secondary">Appointment Id</th>
+      <th scope="col" class="text-secondary">Patient Name</th>
+      <th scope="col" class="text-secondary">Dentist Name</th>
+      <th scope="col" class="text-secondary">Room Number</th>
     </tr>
   </thead>
-  <tbody class="table-group-divider">
+  <tbody>
       <?php echo "$row"; ?>
       
   </tbody>
