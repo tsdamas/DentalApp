e406865ad7bc0e17e6cb5a4d57f7b14781105528
tsdamas <tssdamasceno@gmail.com>
2024-12-03 11:14:40 -0800

    fix validation pattern

--- login.php
+++ login.php
@@ -7,10 +7,6 @@
         <title>User Login Form</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" href="./stylesheet.css">
-    </head>
-<style>
-.error {color: #FF0000;}
-</style>
 </head>
 <body class="bg-body-tertiary">  
         <div class="container-fluid vh-100 d-flex">
@@ -39,11 +35,11 @@
                             <label for="validationCustomUsername" class="form-label username-label">Username</label>
                             <div class="input-group has-validation">
                                 <span class="input-group-text" id="inputGroupPrepend">@</span>
-                                <input type="text" name="username" pattern="/^[a-zA-Z ]{3,15}$/" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
+                                <input type="text" name="username" pattern="^[A-Za-z\s]{1,15}$" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
                          </div>
                         <div class="mb-3">
                             <label for="password-label" class="form-label">Password</label>
-                            <input type="password" name="password" pattern="/^[a-zA-Z0-9]{4,8}$/" class="form-control" id="password-login-field" required >
+                            <input type="password" name="password" pattern="^[a-z]{3,}$" class="form-control" id="password-login-field" required>
                         </div>
                         <button type="submit" name="submit" value="login" class="btn btn-primary">Submit</button>
                     </form>
@@ -52,27 +48,7 @@
             </div>
             </div>
         </div>
-         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-         <script>
-             (() => {
-                 'use strict';
-                 // Fetch all the forms we want to apply custom Bootstrap validation styles to
-                    const forms = document.querySelectorAll('.needs-validation');
-                 // Loop over them and prevent submission
-  
-                Array.from(forms).forEach(form => {
-                form.addEventListener('submit', event => {
-                    if (!form.checkValidity()) {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-
-                    form.classList.add('was-validated');
-                }, false);
-                });
-            })();
-             </script>
-             
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>          
 </body>
 </html>
 
